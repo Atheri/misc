2b3635218340de4b5d7f5ffcd1b7c41e3653d4db
Atheri <corylotze@gmail.com>
2015-07-24 10:49:52 -0700

    Moved strings to squash to the top of program

--- NameSquash/name_squash.py
+++ NameSquash/name_squash.py
@@ -1,3 +1,6 @@
+n1 = 'Cory'
+n2 = 'Alexxa'
+
 def squash_strings(n1, n2):
     name_list = []
     for i in range(0, len(n1)):
@@ -5,8 +8,8 @@ def squash_strings(n1, n2):
             name_list.append(n1[:i+1] + n2[j:].lower())
     return name_list
 
-name_list = squash_strings('Cory', 'Alexxa')
-name_list.extend(squash_strings('Alexxa','Cory'))
+name_list = squash_strings(n1, n2)
+name_list.extend(squash_strings(n2, n1))
 
 for name in name_list:
     print(name)
